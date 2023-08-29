82cd5a3353464192e204c2cb4cf1debb1eb37707
theodoro-freire <theodorofreirelima@gmail.com>
2023-08-29 11:47:54 -0300

    Aula Python 29/08/2023

---
+++ 29.08.23/ola.py
@@ -0,0 +1,33 @@
+#num_real = 5.60
+#print(num_real)
+
+#seu_nome = input("Digite o seu nome:")
+#seu_nome += " Junior"
+#print(seu_nome)
+
+#num1 = 12
+#num2 = 25
+
+#num1 += 2
+
+#res = num1 + num2
+
+#converter para string
+#res_str = str(res)
+
+#converter para int
+#res_int = int(res_str)
+
+#print(res_int + num2)
+
+nome = str(input("Digite seu nome:"))
+#idade = int(input("Digite a sua idade:"))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite o seu peso: "))
+imc = peso / (altura**2)
+
+
+#print("Sua idade é ",idade)
+
+print("Olá, ",nome)
+print("Seu IMC é: ", imc)
